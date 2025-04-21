f1f185a33a00c51595e7f5efc1b367535b2750ba
Popkid-254 <popkidtechs@gmail.com>
2025-04-21 11:28:53 +0300

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -24,7 +24,7 @@ const alive = async (m, Matrix) => {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363391979107532@newsletter',
+        newsletterJid: '120363369453603973@newsletter',
         newsletterName: "Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ",
         serverMessageId: 143
       }

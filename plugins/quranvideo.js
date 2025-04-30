12b1b2aea33fed44356427bd09511e43f76b2839
Bright_Leizer📌 <blacktappy5@gmail.com>
2025-04-30 12:13:33 +0300

    𝚇𝙴𝙾𝙽-𝚇𝙼𝙳

---
+++ plugins/quranvideo.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import config from '../../config.cjs';
+
+const quranVideo = async (m, gss) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const validCommands = ['quranvid', 'qvid', 'quranvideo'];
+
+  if (validCommands.includes(cmd)) {
+    const videoUrl = `https://bk9.fun/Islam/quranvid`;
+await m.React('⏳'); // React with a loading icon
+    await gss.sendMessage(
+      m.from,
+      {
+        video: { url: videoUrl },
+        caption: `📖 *🅧ᴇᴏɴ 🅧м∂ ǫᴜʀᴀɴ ᴠɪᴅᴇᴏs* 📖\n\n¢яєαтє∂ ву Ⴊl𐌀Ꮳk𐌕𐌀ႲႲჄ`,
+      },
+      { quoted: m }
+    );
+  }
+};
+
+export default quranVideo;
